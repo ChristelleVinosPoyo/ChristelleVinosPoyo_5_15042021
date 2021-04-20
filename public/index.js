b342cc373f0184c91cfba01c94399883993c9851
Christelle <christellevinospoyo@gmail.com>
2021-04-20 16:45:21 +0200

    images produit en page d'accueil

--- public/index.js
+++ public/index.js
@@ -1,9 +1,12 @@
 const urlApi = "http://localhost:3000/api/cameras/";
-//pour appeler un objet camera, mettre le num de l'id après l'url de api
+//pour appeler un objet via l'API, il faut mettre le num de l'id après l'url de api :
 const objet0 = "http://localhost:3000/api/cameras/5be1ed3f1c9d44000030b061";
 let resultatsAPI;
-const eltTest = document.querySelector(".test")
-const bannerImg = document.getElementById("banner__img");
+const imgProduit0 = document.querySelector(".img-produit0");
+const imgProduit1 = document.querySelector(".img-produit1");
+const imgProduit2 = document.querySelector(".img-produit2");
+const imgProduit3 = document.querySelector(".img-produit3");
+const imgProduit4 = document.querySelector(".img-produit4");
 
 fetch(urlApi)
 .then((response) => {
@@ -13,10 +16,20 @@ fetch(urlApi)
     //console.log(data);
     resultatsAPI = data;
     console.log(resultatsAPI);
-    //eltTest.innerText = resultatsAPI[0].description;
-    //console.log(eltTest);
-    bannerImg.setAttribute("src", resultatsAPI[4].imageUrl);
+    imgProduit0.setAttribute("src", resultatsAPI[0].imageUrl);
+    imgProduit1.setAttribute("src", resultatsAPI[1].imageUrl);
+    imgProduit2.setAttribute("src", resultatsAPI[2].imageUrl);
+    imgProduit3.setAttribute("src", resultatsAPI[3].imageUrl);
+    imgProduit4.setAttribute("src", resultatsAPI[4].imageUrl);
 })
 
+// page Camera0 _______________________________________________________
 
+//paneau déroulant pour affichier les options de lentilles
+const options = document.querySelector(".options");
+const optionsLi = document.querySelector(".camera0__options li")
 
+options.addEventListener("click", function(){
+    optionsLi.style.display = "block"
+})
+ 
