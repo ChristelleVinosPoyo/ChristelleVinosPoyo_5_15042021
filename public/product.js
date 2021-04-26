4d115cafa9f6f4b923651858a6b596115fb69464
Christelle <christellevinospoyo@gmail.com>
2021-04-26 15:47:13 +0200

    envoi produits vers localstorage, gestion des doublons, OK

--- public/product.js
+++ public/product.js
@@ -11,8 +11,6 @@ fetch(`http://localhost:3000/api/cameras/${id}`)
   })
   .then((data) => {
 
-    console.log(data);
-
     // affichage des données de la fiche produit
     document.querySelector(".product-sheet__container").innerHTML = `
         <div class="product"> 
@@ -27,6 +25,7 @@ fetch(`http://localhost:3000/api/cameras/${id}`)
                     <div class="options">
                         <span class="option option1">Objectif ${data.lenses[0]}</span>
                         <span class="option option2">Objectif ${data.lenses[1]}</span>
+                        <span class="option option2">Objectif ${data.lenses[2]}</span>
                     </div>
                 </div>
                 <span>${data.price} €</span>
@@ -37,53 +36,68 @@ fetch(`http://localhost:3000/api/cameras/${id}`)
         `;
 
     // menu déroulant : selection d'une option -------------> else if ne marche pas 
-    const option = document.querySelectorAll(".option");
-    for (let i = 0; i < option.length; i++) {
-        option[i].addEventListener("click", function() {
-            if (option[i].style.background = "none"){
-                option[i].style.background = "#1f3528";
-                option[i].style.color = "white"
-            }else if(option[i].style.background = "#1f3528"){
-                option[i].style.background = "none";
-                option[i].style.color = "#17271e"
-            }
-        });
-    }
+    let options = document.querySelectorAll(".option");
+
+    
+    for (let option of options) {
+        option.addEventListener("click", function(){
+                if (option.style.background = "none"){
+                    option.style.background = "#1f3528";
+                    option.style.color = "white"
+                }else if(option.style.background = "#1f3528"){
+                    option.style.background = "none";
+                    option.style.color = "#17271e"
+                }
+                if (option === "undefined"){
+                    option.style.visibility = "hidden"
+                }
+        }
+        ) 
+    }   
+    
+
+    // for (let i = 0; i < option.length; i++) {
+    //     option[i].addEventListener("click", function() {
+    //         if (option[i].style.background = "none"){
+    //             option[i].style.background = "#1f3528";
+    //             option[i].style.color = "white"
+    //         }else if(option[i].style.background = "#1f3528"){
+    //             option[i].style.background = "none";
+    //             option[i].style.color = "#17271e"
+    //         }
+    //     });
+    // }
     // ________________ localstorage ______________________________________________________________________
     
     const button = document.querySelector(".button-panier");
     
     button.addEventListener("click", function(){
         let inLocalStorage = JSON.parse(localStorage.getItem("ProductsInLocalStorage")); //conversion en JS de l'objet "productInLocalStorage" récupéré sur localstorage
-        
+        //console.log(inLocalStorage);
+
         if (!inLocalStorage){ // s'il n'y a aucun produits enregistrés dans localstorage
             let inLocalStorage = []
+            data.quantity = 1;
             inLocalStorage.push(data);
             localStorage.setItem("ProductsInLocalStorage", JSON.stringify(inLocalStorage)) //envoi du nouveau produit selectionnée vers localstorage au format JSON
         }
         else if (inLocalStorage){ // s'il y a déjà des produits enregistrés dans localstorage
             
-            let verifDoublons = inLocalStorage.include(data._id) // verifier s'il y a un doublon
-            console.log(verifDoublons);
-            }
-            if (verifDoublons){ // s'il y a un doublon
-                inLocalStorage.push(data);
-                inLocalStorage.quantity ++
-            } 
-            else{ // s'il n'y a pas de doublon
-                inLocalStorage.push(data);
-                inLocalStorage.quantity = 1;
-            }
-            localStorage.setItem("ProductsInLocalStorage", JSON.stringify(inLocalStorage)) ;
-            console.log(inLocalStorage);
-        })
-        })
+            //let indexOfDoublon = inLocalStorage.indexOf(data); //retourne le 1er index trouvé correspondant à data
+            let indexOfDoublon = inLocalStorage.findIndex(element => element._id === data._id);
+            //console.log(indexOfDoublon);
+            console.log(`index du doublon : ${indexOfDoublon}`);
 
-  /*let monObjet = {
-      date : "aujourd'hui",
-      quantité : 3
-  }
-  console.log(monObjet);
-  monObjet.nouvelleCle = null;
-  monObjet.nouvelleCle += 1;
-  console.log(monObjet);*/
+            if (indexOfDoublon === -1){ //si le produit à ajouter n'existe pas dans inLocalStorage
+                data.quantity = 1;
+                inLocalStorage.push(data);
+                localStorage.setItem("ProductsInLocalStorage", JSON.stringify(inLocalStorage)) 
+            }
+            else if(indexOfDoublon >= 0){ //si le produit à ajouter existe déjà dans inLocalStorage
+                inLocalStorage[indexOfDoublon].quantity ++;
+                console.log(`quantité du produit : ${inLocalStorage[indexOfDoublon].quantity}`);
+                localStorage.setItem("ProductsInLocalStorage", JSON.stringify(inLocalStorage)) 
+            }
+        } 
+        })
+    })
