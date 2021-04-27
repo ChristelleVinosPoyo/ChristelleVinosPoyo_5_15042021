4a0636975ef3d47d765daf292af7b9135d4f6a38
Christelle <christellevinospoyo@gmail.com>
2021-04-27 20:45:37 +0200

    corrections : prix format euros, calcul total commande

--- public/panier.js
+++ public/panier.js
@@ -1,29 +1,38 @@
 let inLocalStorage = JSON.parse(localStorage.getItem("ProductsInLocalStorage"));
-console.log(inLocalStorage);
+
+let tabTotalOrder = []; // pour calcul de la somme totale de la commance
 
 for(let product of inLocalStorage){
+
+    // mise en forme du prix au format euros
+    let cameraPrice = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(product.price/100);
+    let cameraTotalPrice = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(product.quantity*product.price/100);
+    
+    tabTotalOrder.push(product.quantity*product.price);
+    
     document.querySelector(".panier__container").innerHTML +=
     
     `<div class="camera">
         <div class="camera__name order-line">${product.name}</div>
         <div class="camera__quantity order-line">${product.quantity}</div>
-        <div class="camera__price order-line">${product.price}€</div>
-        <div class="camera__total order-line">${product.quantity*product.price}€</div>
-    </div>
-
-    <div class="total-order">
+        <div class="camera__price order-line">${cameraPrice}</div>
+        <div class="camera__total order-line">${cameraTotalPrice}</div>
     </div>`
 }
+let somme = tabTotalOrder.reduce((a, b)=> a + b,0); // total de la commance en centimes
+let totalOrderPrice = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(somme/100); //conversion au formar "euros"
+document.querySelector(".somme").innerHTML = `${totalOrderPrice}`;
+
 
 // somme TOTAL de la commande _____________________________
 
-let cameraTotal = document.querySelectorAll(".camera__total"); // la methode reduce ne peut pas être appliquée directement au tableau cameraTotal
-let tabCameraTotal = []; // création d'un tableau simple qui ne contiendra que les prix (innerHtml récupérés avec la boucle ci dessous)
-for (let object of cameraTotal){
-    let price = parseFloat(object.innerHTML); //object.innerHTML est une chaine de caractère. Convertis en nombre avec parseFloat
-    tabCameraTotal.push(price)
-}
-let somme = tabCameraTotal.reduce((a, b)=> a + b,0); 
-console.log(somme);
+// let cameraTotal = document.querySelectorAll(".camera__total"); // la methode reduce ne peut pas être appliquée directement au tableau cameraTotal
+// let tabCameraTotal = []; // création d'un tableau simple qui ne contiendra que les prix (innerHtml récupérés avec la boucle ci dessous)
+// for (let object of cameraTotal){
+//     let price = parseFloat(object.innerHTML); //object.innerHTML est une chaine de caractère. Convertis en nombre avec parseFloat
+//     tabCameraTotal.push(price)
+// }
+// let somme = tabCameraTotal.reduce((a, b)=> a + b,0); 
+// console.log(somme);
 
-document.querySelector(".somme").innerHTML = `${somme}`;
+// document.querySelector(".somme").innerHTML = `${somme}€`;
